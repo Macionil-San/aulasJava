64dfba7310b28b8f7bff07bce9e4a746a0bc98da
Ruan Macionil <macionilruan@gmail.com>
2024-10-03 09:30:55 -0300

    Fazendo Exercicios02 e Exercicio03

---
+++ src/Exercicios/Exercicio03.java
@@ -0,0 +1,24 @@
+package Exercicios;
+
+public class Exercicio03 {
+    public static void main(String[] args) {
+        int num01 = 3, num02 = 7;
+
+        boolean resposta;
+
+        resposta = num01>num02;
+        System.out.println(num01 + " > " + num02 + resposta);
+        resposta = num01<num02;
+        System.out.println(num01 + " < " + num02 + resposta);
+        resposta = num01>=num02;
+        System.out.println(num01 + " >= " + num02 + resposta);
+        resposta = num01<=num02;
+        System.out.println(num01 + " <= " + num02 + resposta);
+
+        resposta = num01 != num02;
+        System.out.println(num01 + " != " + num02 + resposta);
+
+
+
+    }
+}
---
+++ src/Exercicios/testes.java
@@ -0,0 +1,19 @@
+package Exercicios;
+
+public class testes {
+    public static void main(String[] args) {
+        int idade;
+        float altura;
+        double peso;
+        char letra = 66;                 //      aqui o char pega o decimal e transforma em caracter    //
+        int soma, numero1 = 2, numero2 = 4;
+
+        idade = 16;
+        altura = 178;
+        peso = 67;
+        soma = 10;
+        soma = numero1 + numero2;
+
+        System.out.println(letra);
+    }
+}
