7f4fdb9d38b8b2eadafcacdadc5c5ce93ddf8088
Ruan Macionil <macionilruan@gmail.com>
2024-10-24 09:19:57 -0300

    aula05.02

---
+++ src/auala05/exercicios/exercicio01.java
@@ -0,0 +1,21 @@
+package auala05.exercicios;
+
+import java.util.Scanner;
+
+public class exercicio01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int idade = 0;
+
+        while (idade < 15) {
+
+            idade = scanner.nextInt();
+            System.out.println(idade);
+
+        }
+
+        System.out.println("você é maior de idade");
+        scanner.close();
+    }
+}
